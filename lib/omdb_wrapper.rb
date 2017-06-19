9a079e5a311a2c85794015a158f20baa31323897
finn-francis <finn_francis@yahoo.com>
2017-06-19 15:02:13 +0100

    cleaned it up

--- lib/omdb_wrapper.rb
+++ lib/omdb_wrapper.rb
@@ -5,13 +5,13 @@ require 'omdb_wrapper/movie'
 module OmdbWrapper
   class << self
     attr_writer :configuration
-  end
 
-  def self.configuration
-    @configuration ||= Configuration.new
-  end
+    def configuration
+      @configuration ||= Configuration.new
+    end
 
-  def self.configure
-    yield configuration
+    def configure
+      yield configuration
+    end
   end
 end
